64c65ef1d437cb17519a086d99fff93b75be67a1
Sam Cackett <sam.cackett@moonpig.com>
2018-09-21 14:57:16 +0100

    Add tests for distinct states

--- app/src/main/java/com/moonpig/mvisample/mvibase/BaseViewModel.kt
+++ app/src/main/java/com/moonpig/mvisample/mvibase/BaseViewModel.kt
@@ -2,18 +2,14 @@ package com.moonpig.mvisample.mvibase
 
 import android.arch.lifecycle.ViewModel
 import io.reactivex.Observable
-import io.reactivex.functions.BiFunction
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
 
-abstract class BaseViewModel<I, VS : BaseViewState, R : BaseResult<VS>>(private val baseUseCase: BaseUseCase<I, R>) :
+abstract class BaseViewModel<I, VS : BaseViewState, R : BaseResult<VS>>(private val baseIntentProcessor: BaseIntentProcessor<I, R>) :
         ViewModel() {
 
     private val intentSubject = PublishSubject.create<I>()
     private val viewStateObservable by lazy(::compose)
-    private val reducer = BiFunction<VS, R, VS> { previousViewState, result ->
-        result.reduce(previousViewState)
-    }
 
     fun bindIntents(intents: Observable<I>) {
         intents.subscribe(intentSubject)
@@ -25,8 +21,10 @@ abstract class BaseViewModel<I, VS : BaseViewState, R : BaseResult<VS>>(private 
 
     private fun compose(): Observable<VS> {
         return BehaviorSubject.create<VS>().apply {
-            intentSubject.map { baseUseCase.processIntent(it) }
-                    .scan(initialViewState(), reducer)
+            intentSubject.map { baseIntentProcessor.resultFrom(it) }
+                    .scan(initialViewState()) { previousViewState, result ->
+                        result.reduce(previousViewState)
+                    }
                     .distinctUntilChanged()
                     .subscribe(this)
         }
@@ -35,8 +33,8 @@ abstract class BaseViewModel<I, VS : BaseViewState, R : BaseResult<VS>>(private 
     abstract fun initialViewState(): VS
 }
 
-interface BaseUseCase<I, R> {
-    fun processIntent(intent: I): R
+interface BaseIntentProcessor<I, R> {
+    fun resultFrom(intent: I): R
 }
 
 interface BaseResult<VS> {
--- app/src/test/java/com/moonpig/mvisample/mvibase/BaseViewModelTest.kt
+++ app/src/test/java/com/moonpig/mvisample/mvibase/BaseViewModelTest.kt
@@ -8,46 +8,84 @@ internal class BaseViewModelTest {
 
     @Test
     fun shouldBindInitialIntentAndReturnInitialViewState() {
-        val testUseCase = TestUseCase()
-        val testIntent = TestIntent.Test
+        val testUseCase = TestIntentProcessor()
         val testViewModel = TestViewModel(testUseCase)
 
         val testObserver = testViewModel.viewState().test()
-        testViewModel.bindIntents(Observable.just(testIntent))
+        testViewModel.bindIntents(Observable.just(TestIntent.First))
 
         assertThat(testObserver.hasSubscription()).isTrue()
-        assertThat(testObserver.values().first()).isEqualTo(TestViewState.Idle())
-        assertThat(testObserver.values()[1]).isEqualTo(TestViewState.Changed)
+        assertThat(testObserver.values()[0]).isEqualTo(TestViewState.Idle)
+        assertThat(testObserver.values()[1]).isEqualTo(TestViewState.First)
+    }
+
+    @Test
+    fun shouldNotEmitMultipleViewStates_whenNoChanges() {
+        val testUseCase = TestIntentProcessor()
+        val testViewModel = TestViewModel(testUseCase)
+
+        val testObserver = testViewModel.viewState().test()
+        testViewModel.bindIntents(Observable.merge(Observable.just(TestIntent.First), Observable.just(TestIntent.First)))
+
+        assertThat(testObserver.hasSubscription()).isTrue()
+        assertThat(testObserver.values().size).isEqualTo(2)
+        assertThat(testObserver.values()[0]).isEqualTo(TestViewState.Idle)
+        assertThat(testObserver.values()[1]).isEqualTo(TestViewState.First)
+    }
+
+    @Test
+    fun shouldEmitMultipleViewState_whenChanges() {
+        val testUseCase = TestIntentProcessor()
+        val testViewModel = TestViewModel(testUseCase)
+
+        val testObserver = testViewModel.viewState().test()
+        testViewModel.bindIntents(Observable.merge(Observable.just(TestIntent.First), Observable.just(TestIntent.Second)))
+
+        assertThat(testObserver.hasSubscription()).isTrue()
+        assertThat(testObserver.values().size).isEqualTo(3)
+        assertThat(testObserver.values()[0]).isEqualTo(TestViewState.Idle)
+        assertThat(testObserver.values()[1]).isEqualTo(TestViewState.First)
+        assertThat(testObserver.values()[2]).isEqualTo(TestViewState.Second)
     }
 }
 
 sealed class TestIntent {
-    object Test : TestIntent()
+    object First : TestIntent()
+    object Second : TestIntent()
 }
 
-class TestUseCase : BaseUseCase<TestIntent, TestResult> {
-    override fun processIntent(intent: TestIntent): TestResult {
-        return TestResult.InitialResult
-    }
+class TestIntentProcessor : BaseIntentProcessor<TestIntent, TestResult> {
+    override fun resultFrom(intent: TestIntent): TestResult =
+            when (intent) {
+                TestIntent.First -> TestResult.First
+                TestIntent.Second -> TestResult.Second
+            }
 }
 
-class TestViewModel(testUseCase: BaseUseCase<TestIntent, TestResult>) :
-        BaseViewModel<TestIntent, TestViewState, TestResult>(testUseCase) {
+class TestViewModel(testIntentProcessor: BaseIntentProcessor<TestIntent, TestResult>) :
+        BaseViewModel<TestIntent, TestViewState, TestResult>(testIntentProcessor) {
 
     override fun initialViewState(): TestViewState {
-        return TestViewState.Idle()
+        return TestViewState.Idle
     }
 }
 
 sealed class TestResult : BaseResult<TestViewState> {
-    object InitialResult : TestResult() {
+    object First : TestResult() {
         override fun reduce(previousViewState: TestViewState): TestViewState {
-            return TestViewState.Changed
+            return TestViewState.First
+        }
+    }
+
+    object Second : TestResult() {
+        override fun reduce(previousViewState: TestViewState): TestViewState {
+            return TestViewState.Second
         }
     }
 }
 
 sealed class TestViewState : BaseViewState {
-    data class Idle(val testVal: Int = 4) : TestViewState()
-    object Changed : TestViewState()
+    object Idle : TestViewState()
+    object First : TestViewState()
+    object Second : TestViewState()
 }
