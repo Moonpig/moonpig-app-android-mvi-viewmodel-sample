cf795ab8a9ae720788de91e308169289be05da43
Jakob de Comtes <jakob.decomtes@moonpig.com>
2018-12-05 13:22:59 +0000

    Push disposables into BaseActivity

--- app/src/main/java/com/moonpig/mvisample/productdetail/ProductDetailActivity.kt
+++ app/src/main/java/com/moonpig/mvisample/productdetail/ProductDetailActivity.kt
@@ -1,21 +1,18 @@
 package com.moonpig.mvisample.productdetail
 
-import android.app.Activity
 import android.os.Bundle
 import android.view.View
 import com.moonpig.mvisample.R
+import com.moonpig.mvisample.mvibase.BaseActivity
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_product_detail.progressBar
 
-class ProductDetailActivity : Activity(), ProductDetailView {
+class ProductDetailActivity : BaseActivity(), ProductDetailView {
 
     private val viewModel: ProductDetailViewModel by lazy {
         ProductDetailViewModelFactory().create(ProductDetailViewModel::class.java)
     }
     private val productDetailRenderer = ProductDetailRenderer()
-    private val disposables = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,10 +26,6 @@ class ProductDetailActivity : Activity(), ProductDetailView {
                 .subscribe(::renderStateToView)
                 .addToDisposables()
     }
-
-
-    private fun Disposable.addToDisposables() =
-            disposables.add(this)
 
     private fun renderStateToView(viewState: ProductDetailViewState) =
             productDetailRenderer.render(this, viewState)
