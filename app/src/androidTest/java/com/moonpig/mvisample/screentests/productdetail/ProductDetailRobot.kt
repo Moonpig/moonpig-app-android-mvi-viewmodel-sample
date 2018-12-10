318495d53957452d707bcef40730e9bf1c3719a2
Jakob de Comtes <jakob.decomtes@moonpig.com>
2018-12-10 15:37:35 +0000

    Replace isDisplayed with effective visibility check on productDetailRobot

--- app/src/androidTest/java/com/moonpig/mvisample/screentests/productdetail/ProductDetailRobot.kt
+++ app/src/androidTest/java/com/moonpig/mvisample/screentests/productdetail/ProductDetailRobot.kt
@@ -2,19 +2,20 @@ package com.moonpig.mvisample.screentests.productdetail
 
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.Visibility.GONE
+import android.support.test.espresso.matcher.ViewMatchers.Visibility.VISIBLE
+import android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import com.moonpig.mvisample.R
-import org.hamcrest.CoreMatchers.not
 
 class ProductDetailRobot {
     fun isLoading() {
-        onView(withId(R.id.progressBar)).check(matches(isDisplayed()))
+        onView(withId(R.id.progressBar)).check(matches(withEffectiveVisibility(VISIBLE)))
     }
 
     fun isNotLoading() {
-        onView(withId(R.id.progressBar)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.progressBar)).check(matches(withEffectiveVisibility(GONE)))
     }
 
     fun nameDisplayed(name: String) = apply {
