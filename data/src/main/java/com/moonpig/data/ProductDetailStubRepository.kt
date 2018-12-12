6e4263216fb4f37abb6a987b88c0e7cea4119fb0
Jakob de Comtes <jakob.decomtes@moonpig.com>
2018-12-12 15:11:06 +0000

    Add delay to fake responses
    This is to make the screen's behaviour seem more natural

--- data/src/main/java/com/moonpig/data/ProductDetailStubRepository.kt
+++ data/src/main/java/com/moonpig/data/ProductDetailStubRepository.kt
@@ -5,31 +5,34 @@ import com.moonpig.mvisample.domain.productdetail.AddProductRequest
 import com.moonpig.mvisample.domain.productdetail.ProductDetailRepository
 import com.moonpig.mvisample.domain.productdetail.RepositoryState
 import io.reactivex.Observable
+import java.util.concurrent.TimeUnit
 
 class ProductDetailStubRepository : ProductDetailRepository {
     override fun getProductDetails(productId: String): Observable<RepositoryState.GetProductDetail> =
             when (productId) {
-                catArmourProductId -> respondWithCatArmor()
+                catArmourProductId -> respondWithCatArmour()
                 else -> respondWithLoadError()
             }
 
-    private fun respondWithCatArmor() =
-            Observable.just(
-                    RepositoryState.GetProductDetail.InFlight,
-                    RepositoryState.GetProductDetail.Success(
-                            ProductDetail(
-                                    "Advanced Cat Armor",
-                                    "Everything your kitty needs to seriously up it's fighting game! This stylish armor will protect your cat from ordinary piercing and blunt damage. Accessories including the pictured pole not included.",
-                                    299,
-                                    "https://i.postimg.cc/QtwNZdm4/cat-armor-1.jpg"
+    private fun respondWithCatArmour() =
+            Observable.merge(
+                    Observable.just(RepositoryState.GetProductDetail.InFlight),
+                    Observable.just(
+                            RepositoryState.GetProductDetail.Success(
+                                    ProductDetail(
+                                            "Advanced Cat Armour",
+                                            "Everything your kitty needs to seriously up it's fighting game! This stylish armour will protect your cat from ordinary piercing and blunt damage. Accessories including the pictured pole not included.",
+                                            299,
+                                            "https://i.postimg.cc/QtwNZdm4/cat-armor-1.jpg"
+                                    )
                             )
-                    )
+                    ).delay(1, TimeUnit.SECONDS)
             )
 
     private fun respondWithLoadError() =
-            Observable.just(
-                    RepositoryState.GetProductDetail.InFlight,
-                    RepositoryState.GetProductDetail.Error(RuntimeException())
+            Observable.merge(
+                    Observable.just(RepositoryState.GetProductDetail.InFlight),
+                    Observable.just(RepositoryState.GetProductDetail.Error(RuntimeException())).delay(2, TimeUnit.SECONDS)
             )
 
     override fun addProductToBasket(addProductRequest: AddProductRequest): Observable<RepositoryState.AddProduct> =
